07d75130192c558a54d921887d6fd9b6a145d586
aanafiu <abdullahall.nafiu@gmail.com>
2024-09-23 07:01:49 +0600

    ALL donate section complete and Modal on going

--- js/indexfile.js
+++ js/indexfile.js
@@ -1,3 +1,4 @@
+console.log("i am index");
 
 // donation section hide
 document.getElementById("donationBtn").addEventListener("click", function(event){
@@ -25,10 +26,10 @@ document.getElementById("historyBtn").addEventListener("click", function(event){
 
 // Balance add to total donate
 
-document.getElementById('donateNowBtn').addEventListener('click', function(event) {
+document.getElementById('donateNowBtn1').addEventListener('click', function(event) {
     event.preventDefault();
 
-    const donationAmount = getValueByID("donationAmount");
+    const donationAmount = getValueByID("donationAmount1");
 
     let mainBalanceElement = document.getElementById("mainBalance");
     let mainBalance = parseFloat(mainBalanceElement.innerText);
@@ -39,7 +40,76 @@ document.getElementById('donateNowBtn').addEventListener('click', function(event
 
         mainBalanceElement.innerText = (mainBalance - parseFloat(donationAmount));
 
-        let alreadyDonateBalanceElement = document.getElementById('alreadyDonateBalance');
+        let alreadyDonateBalanceElement = document.getElementById('alreadyDonateBalance1');
+        let alreadyDonateBalance = parseFloat(alreadyDonateBalanceElement.innerText);
+        
+        alreadyDonateBalanceElement.innerText = (alreadyDonateBalance + parseFloat(donationAmount));
+
+         my_modal_5.showModal();
+
+         const titleText = this.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+        historyAdd(titleText, donationAmount )
+
+
+    } else {
+
+        my_modal_6.showModal();
+    }
+});
+
+
+// Balance add to total donate btn2
+
+document.getElementById('donateNowBtn2').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const donationAmount = getValueByID("donationAmount2");
+
+    let mainBalanceElement = document.getElementById("mainBalance");
+    let mainBalance = parseFloat(mainBalanceElement.innerText);
+
+
+    // Updating mainBalance btn 2
+    if (!isNaN(donationAmount) && parseFloat(donationAmount) < mainBalance && donationAmount !== "") {
+
+        mainBalanceElement.innerText = (mainBalance - parseFloat(donationAmount));
+
+        let alreadyDonateBalanceElement = document.getElementById('alreadyDonateBalance2');
+        let alreadyDonateBalance = parseFloat(alreadyDonateBalanceElement.innerText);
+        
+        alreadyDonateBalanceElement.innerText = (alreadyDonateBalance + parseFloat(donationAmount));
+
+         my_modal_5.showModal();
+
+         const titleText = this.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+        historyAdd(titleText, donationAmount )
+
+
+    } else {
+
+        my_modal_6.showModal();
+    }
+});
+
+
+
+// Balance add to total donate btn 3
+
+document.getElementById('donateNowBtn3').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const donationAmount = getValueByID("donationAmount3");
+
+    let mainBalanceElement = document.getElementById("mainBalance");
+    let mainBalance = parseFloat(mainBalanceElement.innerText);
+
+
+    // Updating mainBalance btn 3
+    if (!isNaN(donationAmount) && parseFloat(donationAmount) < mainBalance && donationAmount !== "") {
+
+        mainBalanceElement.innerText = (mainBalance - parseFloat(donationAmount));
+
+        let alreadyDonateBalanceElement = document.getElementById('alreadyDonateBalance3');
         let alreadyDonateBalance = parseFloat(alreadyDonateBalanceElement.innerText);
         
         alreadyDonateBalanceElement.innerText = (alreadyDonateBalance + parseFloat(donationAmount));
